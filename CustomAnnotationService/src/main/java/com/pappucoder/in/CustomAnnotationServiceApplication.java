df809ea3f68f4809f0af49ffdba79cad4b22652a
375121 <ajaysharma12105@gmail.com>
2025-04-20 00:06:22 +0530

    Initial Commit CustomAnnotationService Project

---
+++ CustomAnnotationService/src/main/java/com/pappucoder/in/CustomAnnotationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pappucoder.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnnotationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnnotationServiceApplication.class, args);
+	}
+
+}
